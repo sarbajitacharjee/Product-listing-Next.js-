ead69f0bb6a2e16a647892dd89ac4b4adb371f78
Sarbajit Acharjee <sarbajitacharjee712@gmail.com>
2024-06-02 01:15:01 +0530

    commited 2

---
+++ app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Product Listing Page</title>
+        <meta name="description" content="A simple product listing page" />
+        <link rel="icon" href="/favicon.ico" />
+      </>
+    );
+  }
+  
